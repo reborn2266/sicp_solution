8fd25f31df645dcde5650aaba6c9596f09421e5b
mars <reborn2266@gmail.com>
2012-09-30 00:07:30 +0800

    add ex1_5

---
+++ ch01/ex1_5.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define (p) (p))
+
+(define (test x y)
+	(if (= x 0)
+		0
+		y))
+
+#this should be evaluated 0, because p is defined as a function returning itself
+#which means that if use normal order, there is no end of subsitution before evaluation
+(test 0 p)

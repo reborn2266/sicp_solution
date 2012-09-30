fe46083d040aaaa1224967741d69d8d06e5dc370
mars <reborn2266@gmail.com>
2012-09-30 10:29:47 +0800

    add ex1_8 and refine ex1_7

--- ch01/ex1_8.rkt
+++ ch01/ex1_8.rkt
@@ -1,22 +1,17 @@
 #lang racket
 
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
+(define (improve y x)
+  (/ (+ (/ x (* y y)) (* 2 y)) 3.0))
 
 (define (good-enough? x y)
   (and (< (/ x y) 1.001) (> (/ x y) 0.999)))
 
-(define (sqrt-iter guess x)
-  ;; use the author's suggestion, compare the guess and the next improved guess
-  ;; therefore, we can prevent the x's numeric range as possible
+(define (cube-root-iter guess x)
   (if (good-enough? guess (improve guess x))
           guess
-          (sqrt-iter (improve guess x)
+          (cube-root-iter (improve guess x)
                      x)))
 
 ;; following would not be correct before this modification due to
 ;; small number is restricted by epsilon and large number is restricted by float precision
-(sqrt-iter 1.0  0.0005)
+(cube-root-iter 1.1  8)

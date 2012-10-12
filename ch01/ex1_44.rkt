a57c7617654dc332ad22fea0ef510846dd33a87f
reborn2266 <reborn2266@gmail.com>
2012-10-12 13:46:25 +0800

    add ex1_44.rkt

---
+++ ch01/ex1_44.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (repeated f n)
+   (lambda (x)
+     (if (= n 1)
+         (f x)
+         ((compose f (repeated f (- n 1))) x))
+))
+
+(define (compose f g)
+   (lambda (x) (f (g x)))
+)
+
+(define (smooth f dx)
+   (lambda (x) 
+       (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3.0
+       )
+   )
+)
+
+(define (square x) (* x x))
+
+((repeated (smooth square 0.001) 3) 2)

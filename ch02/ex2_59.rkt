cff41c09586bf24eb45a6e2b62ca872d71e416ef
reborn2266 <reborn2266@gmail.com>
2012-10-25 11:12:45 +0800

    reformat

--- ch02/ex2_59.rkt
+++ ch02/ex2_59.rkt
@@ -11,9 +11,11 @@
   (cond ((null? set1) set2)
         ((null? set2) set1)
         ((not (element-of-set? (car set1) set2))
-           (cons (car set1) (union-set (cdr set1) set2)))
+           (cons (car set1) (union-set (cdr set1) set2))
+        )
 	(else (union-set (cdr set1) set2))
-        ))
+  )
+)
 
 (union-set '() '(1 2))
 (union-set '(1 2) '())

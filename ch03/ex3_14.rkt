9aab99524555e61da93ed9e1a2beac59fea5b8b9
reborn2266 <reborn2266@gmail.com>
2012-10-30 13:57:08 +0800

    add ex3_14

---
+++ ch03/ex3_14.rkt
@@ -0,0 +1,18 @@
+#lang r5rs
+
+;; mystery turn x  with its first item only and return its reverse version
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+(display v)
+(define w (mystery v))
+(display w)
+(display v)
+(newline)
